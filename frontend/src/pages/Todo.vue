118ef548fd0f4ed260f13f27f7e7efef47e30989
Mochamad Eka Ramadan <me.ramadan14@gmail.com>
2020-10-21 14:07:39 +0700

    detail task

--- frontend/src/pages/Todo.vue
+++ frontend/src/pages/Todo.vue
@@ -31,6 +31,19 @@
         <q-item-section >
           <q-item-label>{{ todo.text }}</q-item-label>
         </q-item-section>
+        <q-item-section side>
+          <q-btn
+            @click.stop="medium = true, detailTodo(todo)"
+            flat 
+            round 
+            dense 
+            color="primary" 
+            icon="list_alt">
+            <q-tooltip>
+              Detail Task
+            </q-tooltip>
+          </q-btn>
+        </q-item-section>
         <q-item-section v-if="todo.done" side>
           <q-btn
             @click.stop="deleteTodo(todo._id, index)"
@@ -43,17 +56,57 @@
       </q-item>
     </q-list>
     <div
-      v-if="!todos.length"
+      v-if="todos.length == 0"
       class="no-tasks absolute-center">
       <q-icon name="check" size="100px" color="primary"/>
-      <div class="text-h5 text-primary">No Tasks</div>
+      <div class="text-h5 text-primary">No Task</div>
     </div>
+
+    <!-- MODAL DETAIL -->
+    <q-dialog
+      v-model="medium"
+      persistent
+    >
+      <q-card style="width: 700px; max-width: 80vw;">
+        <q-card-section>
+          <div class="text-h6">Detail Task</div>
+        </q-card-section>
+        <q-card-section class="q-pt-none" flat bordered>
+          <q-markup-table :separator="separator">
+            <tbody>
+              <tr>
+                <th class="text-left">Task</th>
+                <td class="text-right">{{ details.text }}</td>
+              </tr>
+              <tr>
+                <th class="text-left">Status</th>
+                <td class="text-right">
+                  <span v-if="details.done">Done</span>
+                  <span v-else>On Progress</span>
+                </td>
+              </tr>
+              <tr>
+                <th class="text-left">Created At</th>
+                <td class="text-right">{{ dateformat(details.createdAt) }}</td>
+              </tr>
+            </tbody>
+          </q-markup-table>
+        </q-card-section>
+
+        <q-card-actions align="right" class="bg-white text-teal">
+          <q-btn flat label="Close" v-close-popup />
+        </q-card-actions>
+      </q-card>
+    </q-dialog>
+
   </q-page>
 </template>
 
 <script>
 import axios from 'axios'
 import { LocalStorage } from 'quasar'
+import _ from 'lodash'
+import moment from "moment";
 
 const urlApi = 'http://localhost:3030/todo'
 const accessToken = LocalStorage.getItem('accessToken')
@@ -67,7 +120,10 @@ export default {
   data (){
     return{
       newTodo: '',
-      todos: []
+      todos: [],
+      medium: false,
+      separator: 'cell',
+      details: ''
     }
   },
   methods: {
@@ -130,8 +186,6 @@ export default {
         }
       }
 
-      console.log(data)
-
       axios
        .patch(urlApi+'/'+id, data, headers)
        .then((res) => {
@@ -140,6 +194,22 @@ export default {
        .catch((err) => {
          console.log(err)
        })
+    },
+    detailTodo (todo) {
+      this.details = todo
+    },
+    dateformat (date) {
+      return moment(date).format('DD MMMM YYYY');
+    },
+    test()
+    {
+      const words = 'lorem ipsum dolor sit amet'
+      var users = [
+        { 'user': 'barney', 'age': 36, 'active': true },
+        { 'user': 'fred',   'age': 40, 'active': false }
+      ];
+
+      console.log(_.map(['6', '8', '10'], _.ary(parseInt, 1)));
     }
 
   },
@@ -147,7 +217,8 @@ export default {
     axios
       .get(urlApi, headers)
       .then((res) => {
-        this.todos = res.data.data
+        console.log(res)
+        this.todos = res.data
       })
       .catch((err) => {
         console.log(accessToken)
